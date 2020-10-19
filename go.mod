5efe3e0ebdee4ec013c7a0219e2003e5457be2c2
mxdillon <marowidi@gmail.com>
2020-10-19 22:53:08 +0100

    feat: add initial function and init go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mxdillon/go-helpers
+
+go 1.15
---
+++ helpers.go
@@ -0,0 +1,14 @@
+package go-helpers
+
+
+// I sIn checks whether an element is present within a string. Change the types to slices as required.
+// Returns a bool and the index of where the match occured (which can be useful).
+func IsIn(b byte, s string) (bool, int) {
+    for i := range s {
+        if s[i] == b {
+            return true, i
+        }
+    }
+    return false, -1
+}
+
