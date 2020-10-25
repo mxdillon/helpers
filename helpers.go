4b4a6d3d4ccc8d48a638479dfebe73f4d21a43fe
mxdillon <marowidi@gmail.com>
2020-10-25 18:24:52 +0000

    test: testing IsIn complete

--- helpers.go
+++ helpers.go
@@ -2,10 +2,10 @@ package helpers
 
 
 // IsIn checks whether an element is present within a string. Change the types to slices as required.
-// Returns a bool and the index of where the match occured (which can be useful).
-func IsIn(b byte, s string) (bool, int) {
+// Returns a bool and the index of where the first match occurred (which can be useful).
+func IsIn(b []byte, s string) (bool, int) {
     for i := range s {
-        if s[i] == b {
+        if s[i] == b[0] {
             return true, i
         }
     }
--- helpers_test.go
+++ helpers_test.go
@@ -4,29 +4,37 @@ import(
     "testing"
 )
 
-// testIsIn
-func testIsIn(t *testing.T) {
+// TestIsIn ensures that various permutations of inputs return the desired tuple.
+func TestIsIn(t *testing.T) {
 	type input struct {
-		b byte
+		b []byte
         s string
 	}
 	type expected struct {
 		b bool
         i int
 	}
+
 	tests := []struct {
 		name      string
 		i      input
 		e      expected
 	}{
-		{"Slice where element is not present", input{byte("c"), "hello max!"}, expected{false, -1}},
+		{"Slice where element is not present", input{[]byte("c"), "hello max!"}, expected{false, -1}},
+		{"Slice where element is present", input{[]byte("m"), "hello max!"}, expected{true, 6}},
+		{"Slice where element is present multiple times", input{[]byte("l"), "hello lollipop lady!"}, expected{true, 2}},
+		{"Slice with multiple characters", input{[]byte("ce"), "nice to meet you"}, expected{true, 2}},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			actualBool, actualInt := IsIn(tt.i.b, tt.i.s)
             if actualBool != tt.e.b{
-				t.Errorf("Boolean incorrect, want %v, got %v", tt.e.b, actualBool)
+				t.Errorf("boolean incorrect, want %v, got %v", tt.e.b, actualBool)
 			}
+            if actualInt != tt.e.i{
+                t.Errorf("index incorrect, want %v, got %v", tt.e.i, actualInt)
+            }
 		})
 	}
 }
